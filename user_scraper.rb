d8034e95e50567045c293bb993fb9b6ef4ddb6e9
Austen Allred <austen.allred@gmail.com>
2014-12-01 04:38:15 -0700

    force uniqueness

--- user_scraper.rb
+++ user_scraper.rb
@@ -1,8 +1,8 @@
 require 'rubygems' 
 require 'twitter'
 
-
-config.consumer_key          = "YOUR_CONSUMER_KEY"
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "YOUR_CONSUMER_KEY"
   config.consumer_secret     = "YOUR_CONSUMER_SECRET"
   config.access_token        = "YOUR_ACCESS_TOKEN"
   config.access_token_secret = "YOUR_ACCESS_SECRET"
@@ -12,39 +12,39 @@ end
 #Enter the name of the file you want the scraped user IDs to go into
 filename = "tweeps.txt" 
 
-
 #Select your keywords to scrape
-keyword1 = "#ifollowback"
-keyword2 = "follow back"
+keyword1 = "follow back"
+keyword2 = "#teamfollowback"
 keyword3 = "#autofollow"
 
 #Select how many user IDs each keyword should scrape
 numberOfTweeps = 5000
-
 #-----------------------------------
 
-puts "Opening the file"
 target = open(filename, 'w')
+ary = []
 
-puts "Grabbing the #{keyword1} tweets"
+puts "Grabbing #{keyword1} tweets"
 client.search("#{keyword1} -rt", result_type: "recent").take(numberOfTweeps).collect do |tweet|
-  target.write(tweet.user.screen_name)
-  target.write("\n")
+  ary.push(tweet.user.screen_name)
 end
 
-puts "Grabbing the #{keyword2} tweets"
+puts "Grabbing #{keyword2} tweets"
 client.search("#{keyword2} -rt", result_type: "recent").take(numberOfTweeps).collect do |tweet|
-  target.write(tweet.user.screen_name)
-  target.write("\n")
+  ary.push(tweet.user.screen_name)
 end
 
-puts "Grabbing the #{keyword3} tweets"
+puts "Grabbing #{keyword3} tweets"
 client.search("#{keyword3} -rt", result_type: "recent").take(numberOfTweeps).collect do |tweet|
-  target.write(tweet.user.screen_name)
+  ary.push(tweet.user.screen_name)
+end
+
+unique = ary.uniq
+unique.each do |uniquetweet|
+  target.write(uniquetweet)
   target.write("\n")
 end
 
-puts "Now closing the file."
 target.close
 
 puts "All finished"
